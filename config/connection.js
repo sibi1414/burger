9493426c293fee19a6bbb27fc1f87514801dba56
Janesz <JaneszS@ccf.org>
2017-10-24 20:10:39 -0400

    burger homework

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+//JawsDB Url
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
   password: "Owen12ha",
   database: "burgers_db"
 });
+
+};
 
 connection.connect(function(err) {
   if (err) {
